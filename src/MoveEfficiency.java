40f36e24a0dd200ba3dec247950e3a7ed11a349c
Zaborstik <zaborstik@gmail.com>
2022-12-12 20:35:45 +0300

    modded

--- src/MoveEfficiency.java
+++ src/MoveEfficiency.java
@@ -4,4 +4,14 @@ public class MoveEfficiency {
     private int numberOfEmptyTiles;
     private int score;
     private Move move;
+
+    public MoveEfficiency(int numberOfEmptyTiles, int score, Move move) {
+        this.numberOfEmptyTiles = numberOfEmptyTiles;
+        this.score = score;
+        this.move = move;
+    }
+
+    public Move getMove() {
+        return move;
+    }
 }
